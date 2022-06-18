b5f010049a33a61b98b5386041a53e7903bee4ec
jaehyeon48 <iamjaehyeon48@gmail.com>
2022-06-18 22:26:19 +0900

    config: 'components' 디렉토리에 대한 path alias 설정

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
     alias: {
       '@src': path.resolve(__dirname, 'src'),
       '@assets': path.resolve(__dirname, 'src', 'assets'),
+      '@components': path.resolve(__dirname, 'src', 'components'),
     },
   },
   module: {
