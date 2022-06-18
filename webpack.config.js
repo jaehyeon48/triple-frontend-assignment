d5ccd110f3f06aa2c4d11a9cc2470e589720d21d
jaehyeon48 <iamjaehyeon48@gmail.com>
2022-06-18 15:26:14 +0900

    feat: 앱 초기 설정
    1. 리액트 프로젝트를 위한 바벨, 웹팩, 타입스크립트 설정
    2. 추후 사용할 이미지 추가
    3. README 파일 추가 및 실행 방법 기재

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src', 'assets'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/i,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.png/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+}
